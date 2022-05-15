a7605c33e5eff12ac2143557cd34c93a995a5687
Tiger <jakkritmontreewong@jakkrit-MacBook-Air.local>
2022-05-16 03:39:39 +0700

    up

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { NavController, LoadingController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
-  constructor() {}
-
+  userID;
+  username;
+  constructor(private storage: Storage,private navCtrl: NavController) {}
+  async ngOnInit() {
+    await this.storage.create();
+    this.getUserData();
+  }
+  async getUserData() {
+    const data = await this.storage.get('userData');
+    const pareData = JSON.parse(data)
+    console.log('data',pareData)
+    if (data != null) {
+      console.log('get', this.username)
+      this.userID = pareData.user_id
+      this.username = pareData.username
+    }
+    if (data == null) {
+      console.log('no', this.username)
+      this.navCtrl.navigateForward("/login")
+    }
+  }
+  logout() {
+    this.storage.clear()
+    this.navCtrl.navigateForward("/login")
+  }
 }
